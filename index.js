5ca149883cbd95679a58c196c9862da049950e40
Justin Palmer <justin@labratrevenge.com>
2012-04-15 11:44:03 -0700

    little more work on example server
    can now render and serve coffeescript files from the examples directory

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 fs = require('fs')
 connect = require('connect')
+coffee = require('coffee-script')
+highlight = require('highlight').Highlight
 
-var app = connect()
+connect()
   .use(connect.logger('dev'))
-  .use(connect.static('public'))
+  .use(connect.static('examples'))
+  .use(connect.static(__dirname))
   .use(connect.favicon())
   .use(connect.directory(__dirname + '/examples'))
   .use(function(req, res, next) {
-    if(req.url.match(/\.(js|css|png|gif|coffee|svg)$/))
+    if(req.url.match(/\.(js|css|png|coffee|gif|svg)$/))
       return next()
 
     fs.readFile(__dirname + "/examples/layout.html", 'utf8', function(err, data) {
       if(err) return next()
 
-      fs.readFile(__dirname + "/examples" + req.url, 'utf8', function(err, data) {
+      var out    = "",
+          view   = req.url,
+          source = __dirname + "/examples" + req.url.replace(/\.html/, '.coffee')
+
+      fs.readFile(source, 'utf8', function(err, coffeedata) {
         if(err) return next()
 
+        var compiled = coffee.compile(coffeedata, {filename: source, bare: false})
         res.writeHead(200, 'OK', {
           'content-type': 'text/html; charset=utf-8',
           'cache-control': 'public, max-age=0'
         })
-        res.end(data.replace('{{{REPLACE_ME}}}', data))
+
+        out = data.replace('{{{SOURCE_ME}}}', highlight(coffeedata))
+        out = out.replace('{{{REPLACE_ME}}}', compiled)
+        res.end(out)
       })
-
     })
-
+  })
+  .use(function(req, res, next) {
+    res.writeHead(404, 'Not Found', {'content-type':'text/html'})
+    res.end("<h1>404 - Not Found</h1>")
   })
  .listen(9292);

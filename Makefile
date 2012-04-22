721e4d52a4faff18e6f27752ce9e48278379a723
Justin Palmer <justin@labratrevenge.com>
2012-04-21 20:47:26 -0700

    complete docco docs

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 test:
 	@./node_modules/.bin/mocha --compilers coffee:coffee-script --require should
+document:
+	@./node_modules/.bin/docco "octo.js"
 
 .PHONY: test
--- octo.js
+++ octo.js
@@ -6,16 +6,14 @@
   var octo = {}
   // The main entry point for interacting with the GitHub API v3.
   //
-  //    var gh = octo.api()
-  //    gh.get('/events').on('success', function(events) {
-  //      console.log(events);
-  //    })
+  //      var gh = octo.api()
+  //      gh.get('/events').on('success', function(events) {
+  //        console.log(events);
+  //      })
   //
-  // Returns `api`
   octo.api = function() {
     var host  = 'https://api.github.com',
         agent = superagent,
-        request,
         limit,
         remaining,
         username,
@@ -24,26 +22,23 @@
 
     function api() {}
 
-    // pager
     function pager(method, path, params) {
-      var req =  superagent[method](api.host() + path),
-          page = 1,
+      var req     = superagent[method](api.host() + path),
+          page    = 1,
           perpage = 30,
-          noop = function() {},
-          events = {
+          noop    = function() {},
+          events  = {
             success: noop,
             error: noop,
             end: noop
           }
 
-      request = function() {
-
+      var request = function() {
         var complete = function(res) {
           limit = ~~res['x-ratelimit-limit']
           remaining = ~~res['x-ratelimit-remaining']
 
           events.end.call(this, res)
-
           if(res.ok)    events.success.call(this, res)
           if(res.error) events.error.call(this, res)
         }
@@ -60,10 +55,22 @@
           .end(complete)
       }
 
-      function pager() {
-        request()
-      }
+      // ### Paging
+      // Each subsequent request for additional pages can easily share the same callbacks and properties.
+      //
+      //      var events = api.get('/events').on('end', function(response) {
+      //        console.log(response.body);
+      //        events.next()
+      //        console.log(events.page());
+      //      })
+      //
+      //      events()
+      //
+      function pager() { request() }
 
+      // Sets or gets the current page
+      //
+      // Returns the pager
       pager.page = function(v) {
         if(!arguments.length) return page
         page = v
@@ -71,6 +78,9 @@
         return pager
       }
 
+      // Sets or gets the items returned per page
+      //
+      // Returns the pager
       pager.perpage = function(v) {
         if(!arguments.length) return perpage
         perpage = v
@@ -78,6 +88,9 @@
         return pager
       }
 
+      // Increments the page number by one and fires a requests for the next page
+      //
+      // Returns the pager
       pager.next = function() {
         page += 1
         request()
@@ -85,6 +98,9 @@
         return pager
       }
 
+      // Decrements the page number by one and fires a request for the previous page
+      //
+      // Returns the pager
       pager.prev = function() {
         page -= 1
         request()
@@ -92,6 +108,15 @@
         return pager
       }
 
+      // Registers a callback for an event
+      //
+      //  Supported events:
+      //
+      // * `success` - Request was successful
+      // * `error` - Request returned an error
+      // * `end` - Request is complete
+      //
+      // Returns a pager
       pager.on = function(event, callback) {
         events[event] = callback
         return pager
@@ -105,40 +130,55 @@
     //
     //      var gh = octo.api().host('https://api.github.com')
     //
+    // Returns the api
     api.host = function(val) {
       if(!arguments.length) return host
       host = val
       return api
     }
 
+    // Initializes a GET request to GitHub API v3
+    // Returns a pager
     api.get = function(path, params) {
       return new pager('get', path)
     }
 
+    // Initializes a POST request to GitHub API v3
+    // Returns a pager
     api.post = function(path, params) {
       return new pager('post', path, params)
     }
 
+    // Initializes a PATCH request to GitHub API v3
+    // Returns a pager
     api.patch = function(path, params) {
       return new pager('patch', path, params)
     }
 
+    // Initializes a PUT request to GitHub API v3
+    // Returns a pager
     api.put = function(path, params) {
       return new pager('put', path, params)
     }
 
+    // Initializes a DELETE request to GitHub API v3
+    // Returns a pager
     api.delete = function(path, params) {
       return new pager('delete', path, params)
     }
 
+    // Returns the API rate limit as reported by GitHub
     api.limit = function() {
       return limit
     }
 
+    // Returns the number of requests that can be made before the `limit` is reached
     api.remaining = function() {
       return remaining;
     }
 
+    // Sets or gets the Basic Auth username
+    // Returns the api
     api.username = function(v) {
       if(!arguments.length) return username;
       username = v
@@ -146,6 +186,8 @@
       return api
     }
 
+    // Sets or gets the Basic Auth password
+    // Returns the api
     api.password = function(v) {
       if(!arguments.length) return password;
       password = v
@@ -153,6 +195,10 @@
       return api
     }
 
+    // Sets or gets an OAuth two token.  You can temporarily use Basic Auth to create a
+    // GitHub Authorization which will grant you an OAuth token.  You can use this token in
+    // your scripts
+    // Returns the api
     api.token = function(v) {
       if(!arguments.length) return token;
       token = v
